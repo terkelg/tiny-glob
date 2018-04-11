21247563f78f8b359a94d8ce5e1c01576ce266ff
Luke Edwards <luke.edwards05@gmail.com>
2018-04-11 00:08:31 -0700

    add benchmark assertion

---
+++ bench/assert.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const { promisify } = require('util');
+const { order } = require('../test/helpers');
+const glob = promisify(require('glob'));
+const fast = require('fast-glob');
+const tiny = require('../src');
+
+let prev;
+
+module.exports = async function (str, opts) {
+	let fn, tmp;
+	for (fn of [glob, fast, tiny]) {
+		tmp = await fn(str, opts).then(order);
+		prev && assert.deepEqual(tmp, prev);
+		prev = tmp;
+	}
+}
